96bc8c3db52f4a531d2acff403508d1066374a43
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-10-05 14:22:31 +0545

    if else elif

--- if_else_and_elif.py
+++ if_else_and_elif.py
@@ -1,6 +1,14 @@
-#odd_even
+# #odd_even
 a=int(input("Enter a number:"))
-if(a%2==0):
-    print("Even")
+# if(a%2==0):
+#     print("Even")
+# else:
+#     print("Odd")
+
+#positive negative or 0
+if(a<0):
+    print("Negative")
+elif(a>0):
+    print("Positive")
 else:
-    print("Odd")
+    print("Zero")
