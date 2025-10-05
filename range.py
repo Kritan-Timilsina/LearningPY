c75b4fefcdb5b6f9cf356db2483113a4c609eda9
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-10-05 22:05:19 +0545

    for loop in python

---
+++ range.py
@@ -0,0 +1,13 @@
+# r=range(5)
+# print (r)
+# l=list(r)
+# print(l)
+# print(type(range))
+
+r=range(10,20)
+l=list(r)
+print(l)
+
+R=range(5,10,20)# range(x,y,z) x,x+z,x+2z,....less than y
+L=list(R)
+print(L)
