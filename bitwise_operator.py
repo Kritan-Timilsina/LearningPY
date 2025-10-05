4fce8a19c8f9d0ecbd0451c722f760c56c15af88
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-10-05 13:27:50 +0545

    Bitwise Operator

---
+++ bitwise_operator.py
@@ -0,0 +1,10 @@
+print(bin(18))
+print(bin(12))
+print(int("0b10010",2))
+print(int("0b1100",2))
+##################################
+#Bit wise operator 
+x=3
+y=5
+print(x&y)
+print(x|y)
---
+++ membership_test_operator.py
@@ -0,0 +1,10 @@
+"""String:Checks for substring
+    Dictionary:check for key
+    List , set , tuple,e.t.c checks for membership
+    """
+l=[10,20,30,40]
+D={"Location":"Kathmandu","Street":"Swyet Binayak"}
+t=(1,2,3,4,5)
+print(30 in l)
+print("Location" not in D)
+print(5 in t)
