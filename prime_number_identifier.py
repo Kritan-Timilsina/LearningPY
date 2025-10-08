e10103f2fe1e8673c610ce300ec0d91b87b693d4
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-10-08 17:10:27 +0545

    Prime number

---
+++ prime_number_identifier.py
@@ -0,0 +1,12 @@
+n=int(input("Enter a number:"))
+def prime(n):
+    flag=0
+    for i in range(2,n):
+        if (n%i==0):
+            flag+=1
+    if flag==0:
+        print("You Entered Prime number.")
+    else :
+        print("You didn't entered prime number")
+prime(n)
+
