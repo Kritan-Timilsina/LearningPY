b39b5150f292eaeef4bbea94e71828ece40fdfd6
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-10-09 22:29:21 +0545

    Default argument

---
+++ default_argument.py
@@ -0,0 +1,9 @@
+def printDetails(id,name="NA",Price="NA"):
+    print(f"Id is {id}")
+    print(f"Name is {name}")
+    print(f"Price is {Price}")
+printDetails(101,"abc",100)
+print()
+printDetails(102)
+print()
+printDetails(103,"x,y,z")
