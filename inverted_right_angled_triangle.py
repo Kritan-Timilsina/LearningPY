11f672fcf1633e0b9dc1c04547ec2fecf33d6e45
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-10-22 22:26:30 +0545

    gfg submittable output

--- inverted_right_angled_triangle.py
+++ inverted_right_angled_triangle.py
@@ -1,12 +1,6 @@
 class Solution:
-    def __init__(self,n):
-        self.n=n
-    def printPattern(self):
-        n=self.n
-        #Code here
-        while n>=0:
+    
+    def printPattern(self,n):
+        while n>0:
             print("* "*n)
             n=n-1
-n=int(input())      
-s=Solution(n)
-s.printPattern()
