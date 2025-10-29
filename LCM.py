703fd8d2d07a85f7de628a5bf36048c9edccbd32
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-10-29 22:24:31 +0545

    gfg acceptable solution

--- LCM.py
+++ LCM.py
@@ -6,3 +6,12 @@ while(res<a*b):
         break
     res+=1
 print("Lcm is ",res)
+"""#User function Template for python3
+def LCM(a,b):
+    # Your code here
+    res=max(a,b)
+    while(res<a*b):
+        if res%a==0 and res%b==0:
+            break
+        res+=1
+    return res"""
