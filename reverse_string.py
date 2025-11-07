5ef75484a443d28ea3319f8ec9a49bb6de2e362c
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-11-07 21:18:17 +0545

    list traversal opposite gfg problem

--- reverse_string.py
+++ reverse_string.py
@@ -2,3 +2,12 @@ def reverseString(s):
     #code here
     
     return (s[::-1])
+"""
+#User function Template for python3
+def listTraversalReverse(arr):
+    #Write your code below
+    
+
+ for i in arr[::-1]:
+     print(i,end=" ")
+    #Don't add a new line as it is provided by the driver code"""
