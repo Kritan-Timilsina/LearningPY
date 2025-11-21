a04da4e6eec04ddb3f134adf34fda015eae3a6a7
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-11-21 23:04:33 +0545

    string operator 2

---
+++ string_operator2.py
@@ -0,0 +1,18 @@
+s1="Kritan"
+print(len(s1))
+s2=s1.upper()
+print(s2)
+s3=s2.lower()
+print(s3)
+print(s1.islower())
+print(s2.isupper())
+k1="Is studying python"
+print(k1.startswith("Kritan"))
+print(k1.endswith("Python"))
+print(k1.endswith("python",8,len(k1)))
+print(k1.split())
+s="----Kritan-----"
+print(s.strip("-"))
+print(s.lstrip("-"))
+print(s.rstrip("-"))
+print(s.find(s1))
