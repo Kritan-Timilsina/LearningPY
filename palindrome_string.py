97010c3615f9b0e5fc04cc4d2131361083a2610e
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-11-25 21:58:25 +0545

    palindrome check in a string

---
+++ palindrome_string.py
@@ -0,0 +1,11 @@
+s=input("Enter a string:")
+low=0
+high=len(s)-1
+while low<high:
+    if s[low]!=s[high]:
+        print("No")
+        break
+    low=low+1
+    high=high-1
+else:
+    print("yes")
