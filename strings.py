2e1f49236b5b84c9dbd2ff95525c7ef40fb06a45
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-11-28 22:14:23 +0545

    string slicing problem

--- strings.py
+++ strings.py
@@ -1,3 +1,13 @@
 def findPattern(s,p):
     #code here
     return s.find(p)
+
+
+"""string slicing problem gfg
+#User function Template for python3
+
+def sliceString(s):
+    #Write your code below
+
+ return s[1:len(s)-1:1]
+"""
