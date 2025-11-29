1bfa71c2835db301888c5c44dce4b19da014331d
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-11-29 20:59:54 +0545

    problem 2

--- strings.py
+++ strings.py
@@ -10,4 +10,11 @@ def sliceString(s):
     #Write your code below
 
  return s[1:len(s)-1:1]
+
+ #User function Template for python3
+
+def changeCase(s):
+    print(s.capitalize())
+    # code here to print capitalized,  and then in the upper case
+    print(s.upper())
 """
