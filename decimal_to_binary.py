cf35d6d775c7f634280fb0346ef8adbad5c7aa2f
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-12-01 21:15:40 +0545

    decimal to binary problem

--- decimal_to_binary.py
+++ decimal_to_binary.py
@@ -14,4 +14,19 @@ or
 def DectoBin(n):
 res=bin(n)
 return res[2:]
+
+
+gfg ans
+#User function Template for python3
+#Complete the below function
+def toBinary(n):
+    #Your code here
+    if n==0:
+        return ("0")
+    res=""
+    while n>0:
+        res=res+str(n%2)
+        n=n//2
+    return (res[::-1])
+
 """
