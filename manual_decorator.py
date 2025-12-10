0c833aea9b5d94269febace7a5950bb082559699
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-12-10 20:44:41 +0545

    decorator_ with @

---
+++ manual_decorator.py
@@ -0,0 +1,11 @@
+def icecream():
+    print("Here's your icecream")
+
+def fudge(func):
+    def inner():
+        print("You added fudge in your icecream")
+        func()
+    return inner
+icecream=fudge(icecream)
+"""manually decorator liyeko yo ma chai @ use nagari """
+icecream()
