79ff49562e1d844ca61260b02420050be4bdd707
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-12-12 22:16:00 +0545

    creating instance

---
+++ gfg video decorator.py
@@ -0,0 +1,14 @@
+class Employee:
+    compName="ap"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    @classmethod
+    def setCompName(cls,cName):
+        cls.compName=cName
+
+Employee.setCompName("Hello")
+print(Employee.compName)
+e=Employee("Kritan",20)
+print(e.compName)
