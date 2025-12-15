f8c7ddca50ae85fb6e038f411771493fe7223adf
Kritan-Timilsina <timilsinakritan04@gmail.com>
2025-12-15 19:43:04 +0545

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(self,id,name):
+        self.id=id
+        self.name=name 
+class Employee(Person):
+        def __init__(self,id,name,salary):
+            super().__init__(id,name)
+            self.salary=salary
+        def printDetails(self):
+            print(self.id)
+            print(self.name)
+            print(self.salary)
+e=Employee(101,"Kritan",700000)
+e.printDetails()
