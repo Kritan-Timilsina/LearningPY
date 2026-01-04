9bd2a247b21d04108568bff8fee105571b2c26f7
Kritan-Timilsina <timilsinakritan04@gmail.com>
2026-01-04 22:11:31 +0545

    closing the files

--- File handling.py
+++ File handling.py
@@ -1,12 +1,15 @@
 f=open ("MyData.txt",'r')
 print(f.readline(),end="")
 print(f.readline())
-print(f.readline())# this only prints 4 letters from line 
+print(f.readline(4))# this only prints 4 letters from line 
 
 f1=open("writingfile.txt",'w')
 f1.write("This is first time when i am inserting in file ")
 
 f2=open("writingfile.txt",'a')
 f2.write("Laptop")
-for data in f:
-    print(data)
+"""for data in f:
+    print(data)"""
+f.close()
+f1.close()
+f2.close()
