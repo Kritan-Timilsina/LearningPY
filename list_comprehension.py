54033cb6e2389e0657b52cf7d1f78e00c8ef6eb8
Kritan-Timilsina <timilsinakritan04@gmail.com>
2026-01-24 15:55:41 +0545

    With list comprehension

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,6 +1,11 @@
 #list comprehension
 marks=[20,30,40,50,60,70]
-new_marks=[]
-for x in marks:
-    new_marks.append(x+2)
-    print(new_marks)
+# new_marks=[]
+# for x in marks:
+#     new_marks.append(x+2)
+# print(new_marks)
+
+
+#with list_comprehension
+new_marks=[x+2 for x in marks]
+print(new_marks)
