651d54e72371be3d247150cd6770be0ffb7603be
Kritan-Timilsina <timilsinakritan04@gmail.com>
2026-03-29 21:29:42 +0545

    haat safai optimized prime number

--- back_after_long_time.py
+++ back_after_long_time.py
@@ -46,4 +46,18 @@ def isPrime(n):
         print(f"{n} is not a Prime number")
 
 n=int(input("Enter value of n:"))
-isPrime(n)
+# isPrime(n)
+
+
+def isPrimeOptimized(n):
+    
+    if n<=1:
+        print(f"{n} is not a Prime number")
+        return 
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            print(f"{n} is not a prime number")
+            return 
+    print(f"{n} is a Prime number")
+
+isPrimeOptimized(n)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,36 +1,10 @@
-# Write a Python decorator called track_calls that does the following:
-
-# Counts how many times the decorated function has been called.
-
-# Prints “Call X of <function_name>” before running the function.
-
-# Prints the arguments passed to the function.
-
-# Modifies the return value of the function by multiplying it by 2.
-
-# Works for any function with arguments (*args and **kwargs).
-
-# Uses @ decorator syntax.
-def track_calls(func):
-    count=0
-    def wrapper(*args,**kwargs):
-        nonlocal count
-        result=func(*args,**kwargs)
-        count+=1
-        print(f"{func.__name__} is runned {count} no of times")
-        
-        return result*2
-    return wrapper
-
-
-@track_calls
-def person(*args,**kwargs):
-    print("The given arguments are:")
-    for i in args:
-        print(i)
-    print("The given Keyword Arguments are:")
-    for key,value in kwargs.items():
-        print(f"{key}:{value}")
-
-person ("Kritan",21, Address="Baneshwor",Uni="Pokhara")
-
+def isPrimeOptimized(n):
+    
+    if n<=1:
+        print(f"{n} is not a Prime number")
+        return 
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            print(f"{n} is not a prime number")
+            return 
+    print(f"{n} is a Prime number")
