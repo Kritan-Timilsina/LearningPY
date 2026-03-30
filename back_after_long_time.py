3e3169b1fada488c2834a0f3042feb7e60c6db9f
Kritan-Timilsina <timilsinakritan04@gmail.com>
2026-03-30 21:38:39 +0545

    Checking Prime and Palindrome in a list of number

--- back_after_long_time.py
+++ back_after_long_time.py
@@ -45,7 +45,7 @@ def isPrime(n):
     else:
         print(f"{n} is not a Prime number")
 
-n=int(input("Enter value of n:"))
+#n=int(input("Enter value of n:"))
 # isPrime(n)
 
 
@@ -97,4 +97,41 @@ def isPrime_Palindrome(n):
     else:
         print(f"{n} is not a Palindrome number")
 
-isPrime_Palindrome(n)
+#isPrime_Palindrome(n)
+"""Create a list of at least 5 integers directly in your code.
+Write a function isPrime(n) that checks whether a number is prime using the √n method.
+Write a function isPalindrome(n) that checks whether a number is a palindrome.
+Traverse the list using a loop.
+For each number, print:"""
+
+l=[100,101,500,505,300]
+def isPrime_Palindrome_list(l):
+    for i in l:
+        prime=True
+        palindrome=False
+        if i<=1:
+            prime=False
+        else:
+            for j in range(2,int(i**0.5)+1):
+                if i%j==0:
+                    prime=False
+                    break
+        
+        if str(i)==str(i)[::-1]:
+            palindrome=True
+        if prime:
+            print(f"{i} is a Prime number.\n")
+        else:
+            print(f"{i} is not a Prime number.\n")
+        
+        if palindrome:
+            print(f"{i} is a Palindrome.\n")
+        else:
+            print(f"{i} is not a Palindrome.\n")
+    
+    
+    
+
+
+
+isPrime_Palindrome_list(l)
