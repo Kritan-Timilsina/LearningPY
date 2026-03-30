db74ae456b131f1bb88b1494b9be1ac9127b4d68
Kritan-Timilsina <timilsinakritan04@gmail.com>
2026-03-30 22:06:48 +0545

    Sorting List

--- back_after_long_time_part2.py
+++ back_after_long_time_part2.py
@@ -4,9 +4,42 @@ Print only Prime numbers
 Print only Palindrome numbers
 Print numbers that are both Prime AND Palindrome"""
 from back_after_long_time import isPrime_Palindrome
-l=[100,99,127,33,77,88,85]
+l=[7,99,127,33,77,88,11]
 def SortingList(l):
     for i in l:
         isPrime_Palindrome(i)
         
-SortingList(l)
+#SortingList(l)
+
+def CategorizingList(l):
+    Prime=[]
+    Palindrome=[]
+    PrimeandPalindrome=[]
+    for j in l:
+        prime=True
+        palindrome=False
+        if j<=1:
+            prime=False
+            
+        for k in range(2,int(j**0.5)+1):
+            if j%k==0:
+                prime=False
+                break
+        
+
+        if str(j)==str(j)[::-1]:
+            palindrome=True
+        
+        if prime:
+            Prime.append(j)
+        if palindrome:
+            Palindrome.append(j)
+        if prime and palindrome:
+            PrimeandPalindrome.append(j)
+
+        
+    print(f"Prime numbers are {Prime}")
+    print(f"Palindrome numbers are {Palindrome}")
+    print(f"Prime and Palindrome numbers are {PrimeandPalindrome}")
+
+CategorizingList(l)
